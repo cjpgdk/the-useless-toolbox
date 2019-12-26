11b1acdefa148418caecf24d5bdf8bc93fc8936f
Christian M. Jensen <cmj@cjpg.dk>
2019-12-26 20:13:33 +0100

    Added old C# WinForm app that simply runs a IsMatch from apttern and text inputs.

---
+++ C#/IsMatch Tester/IsMatch Tester/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace IsMatch_Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pattern = textBox1.Text;
+            string input = textBox2.Text;
+            try
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    MessageBox.Show("IsMatch: Yes");
+                }
+                else
+                {
+                    MessageBox.Show("IsMatch: No");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
